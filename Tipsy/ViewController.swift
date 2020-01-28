efc50bfc9f346004872d905abd20f3f3d40f1e1d
qfernandez <35700716+qfernandez@users.noreply.github.com>
2020-01-28 13:46:59 -0800

    Add files via upload
    Had to upload because of error

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Quentin-Allen Velayo Fernandez on 1/28/20.
+//  Copyright © 2020 Quentin-Allen Velayo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLable: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calculate the tip and total
+        
+        let tipPercentages = [0.15,0.18,0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLable.text = String(format: "$%.2f", total)
+    }
+}
+
